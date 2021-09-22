aa5335894c39f16747248efe76edcea8c87a511f
Meeperbunny <mckibbenian@gmail.com>
2021-09-22 10:22:46 -0600

    Updated tests

--- test.cpp
+++ test.cpp
@@ -1,9 +1,20 @@
+#define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 #include "functions_to_implement.cpp"
 
-SECTION("Testing fibonacci", "[fibonacci]") {
-    REQUIRE(NthFibonacci(1) = 0);
-    REQUIRE(NthFibonacci(3) = 1);
-    REQUIRE(NthFibonacci(11) = 55);
-    REQUIRE(NthFibonacci(16) = 610);
+TEST_CASE("Testing fibonacci", "[fibonacci]") {
+    REQUIRE(NthFibonacci(1) == 0);
+    REQUIRE(NthFibonacci(3) == 1);
+    REQUIRE(NthFibonacci(11) == 55);
+    REQUIRE(NthFibonacci(16) == 610);
+    REQUIRE(NthFibonacci(18) == 1597);
+}
+
+TEST_CASE("Testing vector sum", "[vecsum]") {
+    // Create vector
+    std::vector<int> myVecOne{1, 2, 3, 4, 5};
+    std::vector<int> myVecTwo{2, 3, 4, 5, 6};
+
+    REQUIRE(Sum(myVecOne) == 15);
+    REQUIRE(Sum(myVecTwo) == 20);
 }
